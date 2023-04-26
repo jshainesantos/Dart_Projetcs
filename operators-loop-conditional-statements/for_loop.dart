8c7f2d92e943a889af2008b231ce069c92da42fa
Shaine Santos <90233510+jshainesantos@users.noreply.github.com>
2023-04-26 20:11:04 +0800

    Add files via upload

---
+++ operators-loop-conditional-statements/for_loop.dart
@@ -0,0 +1,7 @@
+void main() {
+  List<int> numbers = [1, 2, 3, 4, 5];
+
+  for (int i = 0; i < numbers.length; i++) {
+    print(numbers[i]);
+  }
+}
---
+++ operators-loop-conditional-statements/switch_case.dart
@@ -0,0 +1,18 @@
+void main() {
+  String color = "red";
+
+  switch (color) {
+    case "red":
+      print("The color is red.");
+      break;
+    case "green":
+      print("The color is green.");
+      break;
+    case "blue":
+      print("The color is blue.");
+      break;
+    default:
+      print("The color is not recognized.");
+      break;
+  }
+}
